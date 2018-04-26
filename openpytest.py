8121df815723b97c109e599d0c9d65b959bd5254
rowong20 <rowong20@gmail.com>
2018-04-26 12:09:08 -0700

    Despair
    Giving up on Iteration, manually inserting.

--- openpytest.py
+++ openpytest.py
@@ -1,11 +1,13 @@
 from openpyxl import load_workbook
-wb= load_workbook('test.xlsx')
+import re
 #Instructions
 #This script will move all compiled data and inputed rates into recharges
 #In order to run this script, you must first have a worksheet with the compiled data.
 #Export from the data base the CHW and KWH data (be sure to have column names)
 #Fill in data from reads for Water and Gas
 #Correct any anomalies in the compiled data.
+#Uses openpyxl library for excel sheet manipulation
+#Uses re for easier string removal
 
 # C(july)-N(june) Use input month # to determine which column to input data.
 while True:
@@ -38,14 +40,25 @@ while True:
 
 #Inputing Utility_Summary Code, three sheets to edit
 #Gall R&W Utility Summary, Facilities ... ..., Library ... ...
-data = [wb["Gall R&W Utility Summary"],wb["Facilities Utility Summary"], wb["Library Utility Summary"]]
-print (data)
-ws= data[1]
-ws2 = wb["Gall R&W Utility Summary"]
-wb2= load_workbook('dataCompilation.xlsx')
-ws2 = wb2["Gas"]
-gasInput=ws2['C11']
-ws.columns
+#Currently Cannot think of way to easily iterate, maybe future find a way.
+wb= load_workbook('test.xlsx')
+editSheet = wb["Gall R&W Utility Summary"]
+wb2 = load_workbook('dataCompilation.xlsx')
+#Electricity Input
+# dataSheet = wb2.worksheets[0]
+# actual=dataSheet['C2'].value
+# num_actual=dataSheet['E2'].value
+# num_expected=dataSheet['F2'].value
+# input= 	'='+repr(actual)+'/('+repr(num_actual)+'/'+repr(num_expected)+')'
+# editSheet.cell(row=7, column=inputColumn).value=input
+#Gas Input
+dataSheet = wb2.worksheets[1]
+actual=dataSheet['C11'].value
+actual = re.sub('cuft.', '', actual)
+print(actual)
+#editSheet.cell(row=7, column=inputColumn).value=input
+	
+#gasInput=ws2['C11']
 #print(ws.cell(row=20, column=inputColumn).value)
 #try ws = wb.worksheets[0]
 
@@ -56,4 +69,4 @@ ws.columns
 #Ask Gabriel to format Gas reads to be like Utility Recharges (Months in same columns)
 
 #Save Changes to chosen excel sheet
-wb.save('test.xlsx')
+#wb.save('test.xlsx')
